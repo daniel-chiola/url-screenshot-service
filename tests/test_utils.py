816aa53999f741f79fd9d7f4fcdbaa7bb18593f5
Daniel Chiola <123893690+daniel-chiola@users.noreply.github.com>
2026-09-20 19:05:30 +0200

    test: aggiorna l'estensione attesa a .jpeg

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,9 +11,9 @@ pytestmark = pytest.mark.anyio
 
 def test_url_to_filename():
     """url_to_filename deriva il nome file dall'host dell'URL, sostituendo i punti con underscore."""
-    assert utils.url_to_filename("https://google.com") == "screenshot_google_com.png"
-    assert utils.url_to_filename("http://example.org/path/to/page") == "screenshot_example_org.png"
-    assert utils.url_to_filename("https://sub.domain.co.uk/") == "screenshot_sub_domain_co_uk.png"
+    assert utils.url_to_filename("https://google.com") == "screenshot_google_com.jpeg"
+    assert utils.url_to_filename("http://example.org/path/to/page") == "screenshot_example_org.jpeg"
+    assert utils.url_to_filename("https://sub.domain.co.uk/") == "screenshot_sub_domain_co_uk.jpeg"
 
 
 async def test_is_reachable_url_raggiungibile(monkeypatch):
