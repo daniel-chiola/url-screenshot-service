275df80ce118e88f0f3c8496336e04de6ff83962
Daniel Chiola <123893690+daniel-chiola@users.noreply.github.com>
2026-09-20 09:24:00 +0200

    scaffold: struttura iniziale del progetto (FastAPI + Playwright, Docker, docker-compose)

---
+++ app/utils.py
@@ -0,0 +1,4 @@
+def url_to_filename(url: str) -> str:
+    """Deriva un nome file sicuro dall'URL (es. https://google.com -> screenshot_google_com.png)."""
+    # TODO: implementare la logica di sanificazione/derivazione del nome file
+    raise NotImplementedError
